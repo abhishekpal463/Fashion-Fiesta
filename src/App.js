81798c612e7065d6cd5ec45f6ea2d182d2c6e105
Abhishek Pal <abhishekpal463@gmail.com>
2021-06-25 21:07:45 +0530

    added react-router and HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
 import './App.css';
 import Homepage from './pages/homepage/hompage.component';
+import { Route , Switch} from 'react-router-dom';
+
+const HatsPage = () =>(
+  <div>
+    <h1>Hat Page</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <Homepage/>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/hats" component={HatsPage} />
+    </Switch>
+      
     </div>
+
   );
 }
 
