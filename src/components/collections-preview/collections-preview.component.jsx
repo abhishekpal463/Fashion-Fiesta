2591ee5caa3db8cd73f56e2418b3a6064a89a9ae
Abhishek Pal <abhishekpal463@gmail.com>
2021-06-27 22:38:39 +0530

    updating shopp-page and collection-items

--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import CollectionItem from '../collection-items/collection-item.component';
+
 import './collections-preview.styles.scss';
 
 
 const CollectionPreview = ({title,items}) => (
-    <div className='preview-collection'>
+    <div className='collection-preview'>
         <h1 className='title'> {title.toUpperCase()} </h1>
             <div className='preview'>
                 {items
                 .filter((item,index)=>index<4)
-                .map(item=>(
-                    <div key={item.id}>{item.name}</div>
+                .map(({id , ...otherItemProps})=>(
+                    <CollectionItem key={id}{...otherItemProps}/>
                 ))}
             </div>
     </div>
--- src/pages/shoppage/shop.components.jsx
+++ src/pages/shoppage/shop.components.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SHOP_DATA from "./shop.data";
 import CollectionPreview from '../../components/collections-preview/collections-preview.component';
 
+
 class ShopPage extends React.Component{
     constructor(props){
         super();
