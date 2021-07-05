f36219ac1755c3e66b6d576bf7c232ae2c1ed7c0
Abhishek Pal <abhishekpal463@gmail.com>
2021-07-05 21:03:06 +0530

    Redirecting User

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route , Switch} from 'react-router-dom';
+import { Route , Switch,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import './App.css';
 import Homepage from './pages/homepage/hompage.component';
@@ -44,7 +44,17 @@ class App extends React.Component {
     <Switch>
       <Route exact path="/" component={Homepage} />
       <Route exact path="/shop" component={ShopPage} />
-      <Route exact path="/signin" component={SignInandSignUpPage} />
+      <Route 
+        exact 
+        path="/signin" 
+        render={()=> 
+        this.props.currentUser ? (
+        <Redirect to = '/' />
+        ):(
+        <SignInandSignUpPage/>
+        )
+        }  
+        />
     </Switch>
       
     </div>
@@ -53,8 +63,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser:user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser:user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
